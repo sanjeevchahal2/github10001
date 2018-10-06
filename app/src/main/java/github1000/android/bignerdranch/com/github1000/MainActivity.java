f38a36e98288f58d524b2fd41bc73638eed454a4
sanjeev chahal <s_c141@student.usc.edu.au>
2018-10-06 14:23:35 +1000

    Initial commit

---
+++ app/src/main/java/github1000/android/bignerdranch/com/github1000/MainActivity.java
@@ -0,0 +1,13 @@
+package github1000.android.bignerdranch.com.github1000;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
